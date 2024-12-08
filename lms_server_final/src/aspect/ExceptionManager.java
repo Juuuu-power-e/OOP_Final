a25349bca592472107a26781c6ac0e4965e99281
이승주 <129913026+Juuuu-power-e@users.noreply.github.com>
2024-12-08 18:05:39 +0900

    feat: config 변경사항 로그표시

--- lms_server_final/src/aspect/ExceptionManager.java
+++ lms_server_final/src/aspect/ExceptionManager.java
@@ -2,7 +2,7 @@ package aspect;
 
 public class ExceptionManager {
 	
-	public void process(Exception exception) {
-		exception.printStackTrace();
+	public void process(Exception e) {
+		e.printStackTrace();
 	}
 }
---
+++ student-info-system/src/main/java/com/lms/studentinfosystem/StudentInfoSystemApplication.java
@@ -0,0 +1,8 @@
+package com.lms.studentinfosystem;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class StudentInfoSystemApplication {
+
+}
